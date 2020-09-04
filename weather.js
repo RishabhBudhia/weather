f827d45f0bcc58354e99c540d955f00a926512dd
RishabhBudhia <59286478+RishabhBudhia@users.noreply.github.com>
2020-09-04 08:55:47 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,22 @@
+// function fn2() {
+//     var inpt = document.getElementById("input");
+//     console.log(inpt.value);
+// }
+
+function fn1() {
+    var input = document.getElementById("input");
+    var text_data = document.getElementById("text");
+    var one = document.getElementById("one");
+    var two = document.getElementById("two");
+    var three = document.getElementById("three");
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=1ff66ad72a9ec577b16ffe46c8c5d2f5')
+        .then((res) => res.json()).then((data) => {
+            one.innerHTML = data['main']['temp'];
+            two.innerHTML = data['visibility'];
+            three.innerHTML = data['weather'][0]['description'];
+
+
+        })
+        .catch((err) => alert("wrong name"))
+}
