42e6dd369db8051b392afcf231521faa67c804bb
RishabhBudhia <59286478+RishabhBudhia@users.noreply.github.com>
2020-09-05 13:20:54 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -12,9 +12,11 @@ function fn1() {
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=1ff66ad72a9ec577b16ffe46c8c5d2f5')
         .then((res) => res.json()).then((data) => {
-                one.innerHTML = 'Temp:' + (data['main']['temp'] - (273)).toFixed(2) + '* C';
-                two.innerHTML = 'Visibilty:' + data['visibility'];
-                three.innerHTML = 'Clouds:' + data['weather'][0]['description'];
+                one.innerHTML = `<h3 style="display:inline;font-family:cursive">Temp:</h3> ${(data['main']['temp'] - (273)).toFixed(2)}* C`;
+                five.innerHTML = `<h3 style="display:inline;font-family:cursive">Feels Like:</h3> ${(data['main']['feels_like'] - (273)).toFixed(2)}* C`;
+                four.innerHTML = `<h3 style="display:inline;font-family:cursive">Humidity:</h3> ${(data['main']['humidity'])} % `;
+                two.innerHTML = `<h3 style="display:inline;font-family:cursive">Visibilty:</h3> ${data['visibility']}`;
+                three.innerHTML = `<h3 style="display:inline;font-family:cursive">Clouds:</h3> ${data['weather'][0]['description']}`;
 
 
             }
